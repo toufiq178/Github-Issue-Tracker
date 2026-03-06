d15c4baefeeb1a5b8e30c6fc6782bd2c0bcfdbc4
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-06 21:43:47 +0600

    sign in validation function added && home.html, home.js file create and connect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+
+
+document.getElementById("sign-in-btn").addEventListener("click" , function () {
+    
+
+    const usernameInput = document.getElementById("username-input");
+    const usernameValue = usernameInput.value 
+
+    const passwordInput = document.getElementById("password-input");
+    const passwordValue = passwordInput.value 
+
+    
+    if (usernameValue === "admin" && passwordValue === "admin123") {
+        
+        
+        window.location.assign('./home.html');
+
+    } else{
+
+
+        alert("no")
+    }
+    
+})
