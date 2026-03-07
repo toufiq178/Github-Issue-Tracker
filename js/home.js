fcec3f81721a8f11edb30e7e2a11e33acdc38420
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-07 13:02:42 +0600

    rendaring issues labels class update dynamic

--- js/home.js
+++ js/home.js
@@ -1,18 +1,36 @@
 // console.log("home");
+let allIssueData = []
 
 
 
 
+
+const priorityColor = {
+
+    high: "bg-red-100 text-red-500",
+    medium: "bg-yellow-100 text-yellow-500",
+    low: "bg-green-100 text-green-500"
+}
+
+const labelsColor ={
+
+    "good first issue" : "bg-yellow-100 text-yellow-500",
+    "help wanted" : "bg-yellow-100 text-yellow-500",
+    documentation : "bg-yellow-100 text-yellow-500",
+    enhancement : "bg-green-100 text-green-500",
+    bug : "bg-red-100 text-red-500",
+}
 
 
 
 const createElement = (arr) => {
 
-
-    const htmlElement = arr.map(elem => `<span class="text-xs bg-yellow-100 text-gray-600 px-2 py-1 rounded-full"> ${elem} </span>`)
+    const htmlElement = arr.map(elem => `<span class="text-xs ${labelsColor[elem]} px-2 py-1 rounded-full"> ${elem} </span>`)
     return htmlElement.join(" ")
 
 }
+
+
 
 function togglingIssue(id) {
 
@@ -33,6 +51,22 @@ function togglingIssue(id) {
     });
 }
 
+function filterIssue(status) {
+    
+    let filterIssue = []
+
+    if (status === "all") {
+        
+        filterIssue = allIssueData
+
+    } else {
+
+        filterIssue = allIssueData.filter(issue => issue.status === status);
+    }
+
+    displayAllIssue(filterIssue)
+}
+
 
 
 const loadAllIssue = async () => {
@@ -40,7 +74,9 @@ const loadAllIssue = async () => {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
 
-    displayAllIssue(data.data)
+    allIssueData = data.data
+    displayAllIssue(allIssueData)
+    
 }
 
 
@@ -92,37 +128,34 @@ const loadModalIssue = async (id) => {
 
 
 const displayAllIssue = (issues) => {
-    
-    
-    const issueTab = document.getElementById("issue-card-container");
-    issueTab.innerHTML = ""
-    
-    
-    
+
+
+    const allIssueContainer = document.getElementById("all-issue-container");
+    allIssueContainer.innerHTML = ""
+
+    const allIssue = document.getElementById("all-Issues");
+    allIssue.textContent = `${issues.length} Issues`
+
+
     // 
-    const priorityColor = {
-        
-        high: "bg-red-100 text-red-500",
-        medium: "bg-yellow-100 text-yellow-500",
-        low: "bg-green-100 text-green-500"
-    }
-    
-    
-    
+
+
+
+
     issues.forEach(issue => {
-        
+
         // console.log(issue.labels);
-        
-        
+
+
         const div = document.createElement("div");
-        
-        
+
+
         // priority set 
-        
-        
-        
+
+
+
         div.className = " bg-white rounded-xl shadow-lg border border-gray-200 overflow-hidden"
-        
+
         div.innerHTML = `
 
                             <div onclick = "loadModalIssue(${issue.id})"  class="p-4  border-t-6  ${issue.status === "open" ? "border-green-600" : "border-purple-600"}">
@@ -134,14 +167,14 @@ const displayAllIssue = (issues) => {
                                     <div id = "status-container">
                                     
                                         ${issue.status === "open" ?
-                                            `<div  class="w-6 h-6 bg-green-100  rounded-full ">
+                `<div  class="w-6 h-6 bg-green-100  rounded-full ">
                                                 <img src="./assets/Open-Status.png" alt="">
                                             </div>`
-                                            :
-                                            `<div  class="w-6 h-6 bg-purple-100  rounded-full ">
+                :
+                `<div  class="w-6 h-6 bg-purple-100  rounded-full ">
                                                 <img src="./assets/Closed-Status.png" alt="">
                                             </div>`
-                                        }
+            }
                                                                        
                                     </div>
 
@@ -184,10 +217,10 @@ const displayAllIssue = (issues) => {
                             </div>
         
         `
-        
 
-        
-        issueTab.appendChild(div)
+
+
+        allIssueContainer.appendChild(div)
     });
 
 }
@@ -198,12 +231,7 @@ const displayModalIssue = (modalIssue) => {
     // console.log(modalIssue);
 
     // priority set 
-    const priorityColor = {
 
-        high: "bg-red-100 text-red-500",
-        medium: "bg-yellow-100 text-yellow-500",
-        low: "bg-green-100 text-green-500"
-    }
 
     const modalContainer = document.getElementById("modalContainer");
     modalContainer.innerHTML = `
